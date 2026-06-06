bbeb31f6eb0835c694c1eb90cbbc31438688e8ea
marinehaley3 <marinehaley3@gmail.com>
2026-06-06 18:48:13 +0300

    Update RewardCodeContext.jsx

--- src/Context/RewardCodeContext.jsx
+++ src/Context/RewardCodeContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect } from "react";
+import React, { createContext, useContext } from "react";
 import API from "../api/axios";
 import { AuthContext } from "./AuthContext";
 
@@ -6,23 +6,14 @@ export const RewardCodeContext = createContext();
 
 export const RewardCodeProvider = ({ children }) => {
   const { token } = useContext(AuthContext);
-  const [codes, setCodes] = useState([]);
 
-  useEffect(() => {
-    if (!token) return;
-    API.get("/rewards/my-codes")
-      .then((res) => setCodes(res.data))
-      .catch(console.error);
-  }, [token]);
-
-  const handleRedeem = async (code) => {
-    const res = await API.post("/rewards/redeem", { code });
-    setCodes((prev) => [...prev, res.data.redeemedCode]);
-    return res.data;
+  const redeemCode = async (code) => {
+    const res = await API.post("/rewardcode/redeem", { code });
+    return res.data; // { message, amount, newBalance }
   };
 
   return (
-    <RewardCodeContext.Provider value={{ codes, redeemCode: handleRedeem }}>
+    <RewardCodeContext.Provider value={{ redeemCode }}>
       {children}
     </RewardCodeContext.Provider>
   );
