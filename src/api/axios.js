6d4aaa780fe5e1f0f823019537845b0e431d3e95
marinesaili461 <marinesaili461@gmail.com>
2026-06-19 19:02:23 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://mar1nepanel.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://mar1nepanel.onrender.com/api",
   timeout: 10000,
 });
 
